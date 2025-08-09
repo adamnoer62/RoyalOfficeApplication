952bd34e9bc822d9f48ec1e6bcb997e2fcabba1b
Adam <adam@Adams-MacBook-Pro.local>
2025-08-09 10:29:26 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth')->except(['halamandepan', 'about', 'blog', 'contact']);
+        $this->middleware('auth')->except(['halamandepan', 'about', 'blog', 'contact','layanan']);
     }
     
 
